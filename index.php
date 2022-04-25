5028b0687b07008108c68c15ba0deb721bf8b420
Carlos Villa <ing.cavij@gmail.com>
2022-04-25 14:10:10 -0500

    Add Header, Footer and Table

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php include 'template/header.php' ?>
+
+<div class="container mt-5">
+  <div class="row justify-content-center">
+    <div class="col-md-7">
+      <div class="card">
+        <div class="card-header">
+          Lista de Personas
+        </div>
+        <div class="p-4">
+          <table class="table align-middle">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nombre</th>
+                <th>Edad</th>
+                <th>Signo</th>
+                <th>Opciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td scope="row"></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td scope="row"></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      DOS
+    </div>
+  </div>
+</div>
+
+<?php include 'template/footer.php' ?>
